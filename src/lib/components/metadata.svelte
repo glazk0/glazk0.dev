5c636574a2f105bf5b3031a5b41d5e25eea602a9
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-07-28 17:08:11 +0200

    fix: added async script

--- src/lib/components/metadata.svelte
+++ src/lib/components/metadata.svelte
@@ -57,5 +57,5 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="{CDN_URL}/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="{CDN_URL}/favicon-16x16.png" /> -->
 
-	<script src="https://analytics.glazk0.dev/api/script.js" data-site-id="1" defer></script>
+	<script src="https://analytics.glazk0.dev/api/script.js" data-site-id="1" async defer></script>
 </svelte:head>
