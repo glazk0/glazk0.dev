f205c0c4e400a0d9e511e5ad7d0627756af092bf
glazk0 <43091603+glazk0@users.noreply.github.com>
2025-07-29 08:56:32 +0200

    fix: better analytics placement

--- src/lib/components/metadata.svelte
+++ src/lib/components/metadata.svelte
@@ -56,6 +56,4 @@
 	<!-- <link rel="apple-touch-icon" sizes="180x180" href="{CDN_URL}/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" sizes="32x32" href="{CDN_URL}/favicon-32x32.png" />
 	<link rel="icon" type="image/png" sizes="16x16" href="{CDN_URL}/favicon-16x16.png" /> -->
-
-	<script src="https://analytics.glazk0.dev/api/script.js" data-site-id="1" async defer></script>
 </svelte:head>
